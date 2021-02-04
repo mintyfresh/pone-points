eb646146f098313bfffac20aa61d9f1ca25068ff
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-04 17:10:37 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.0'
+
+gem 'amazing_print'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'faker'
+gem 'oj'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'rails', '~> 6.1.1'
+gem 'sass-rails', '>= 6'
+gem 'seedbank'
+gem 'turbolinks', '~> 5'
+gem 'view_component'
+
+group :development do
+  gem 'annotate'
+  gem 'listen', '~> 3.3'
+  gem 'spring'
+  gem 'web-console', '>= 4.1.0'
+end
+
+group :development, :test do
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem 'dotenv-rails'
+  gem 'factory_bot_rails'
+  gem 'rubocop'
+  gem 'rubocop-performance'
+  gem 'rubocop-rails'
+  gem 'rubocop-rspec'
+end
+
+group :test do
+  gem 'rspec-rails'
+end
---
+++ config/application.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+# require "active_storage/engine"
+require 'action_controller/railtie'
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require 'action_view/railtie'
+# require "action_cable/engine"
+require 'sprockets/railtie'
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module PonePoints
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Disable ActionView's field wrapper divs as they interfere with Bootstrap.
+    config.action_view.field_error_proc = proc { |html_tag| html_tag }
+
+    # Load translations from subdirectories of `config/locales` as well.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    # Dump structure file in PostgreSQL native format.
+    config.active_record.schema_format = :sql
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.orm                 :active_record
+      g.fixture_replacement :factory_bot
+      g.test_framework      :rspec, view_specs: false, request_specs: false
+      g.assets              false
+      g.helper              false
+    end
+  end
+end
