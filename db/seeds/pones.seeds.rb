d7c42c371e2da5d7f18069d53c04bef5921143af
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-04 17:38:13 -0500

    Display boons on pone page.

--- db/seeds/pones.seeds.rb
+++ db/seeds/pones.seeds.rb
@@ -2,4 +2,11 @@
 
 Pone.find_or_create_by!(name: 'Starf') do |pone|
   pone.discord_id = 'Starfflame#0815'
+
+  pone.boons.build(
+    granted_by:   'Minty Fresh',
+    message_link: 'https://discord.com/channels/140359933341204481/140359933341204481/807008644817485854',
+    points_count: 2,
+    occurred_at:  '2020-02-04 17:04:00 +0500'
+  )
 end
