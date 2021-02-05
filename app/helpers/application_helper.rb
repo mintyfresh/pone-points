37294498bf5a654bd59739c7ee5d6b3910bd775d
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-04 22:23:29 -0500

    Add helpers to handle titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def title
+    return content_for(:title) if content_for?(:title)
+
+    t("#{controller_path.tr('/', '.')}.#{action_name}.title", default: 'Good Pone Points')
+  end
 end
