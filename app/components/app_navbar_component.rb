ad0dc42a12ef70f20a9ab35e89fe9f1d0414649f
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-04 20:31:39 -0500

    Add navlink component.

--- app/components/app_navbar_component.rb
+++ app/components/app_navbar_component.rb
@@ -5,4 +5,12 @@ class AppNavbarComponent < ApplicationComponent
   def initialize(current_pone:)
     @current_pone = current_pone
   end
+
+  # @param label [String]
+  # @param link [String, Object]
+  # @param options [Hash]
+  # @return [String]
+  def nav_link(label, link, **options)
+    render(AppNavbarLinkComponent.new(label: label, link: link, **options))
+  end
 end
---
+++ app/components/app_navbar_link_component.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class AppNavbarLinkComponent < ApplicationComponent
+  # @param label [String]
+  # @param link [String, Object]
+  # @param options [Hash]
+  def initialize(label:, link:, **options)
+    @label   = label
+    @link    = link
+    @options = options
+  end
+
+  # @return [Boolean]
+  def current?
+    current_page?(@link)
+  end
+
+  # @return [String]
+  def nav_link_class
+    "nav-link#{' active' if current?}"
+  end
+
+  # @return [Hash]
+  def nav_link_options
+    options = @options
+
+    options = options.merge('aria-current' => 'page') if current?
+
+    options
+  end
+end
