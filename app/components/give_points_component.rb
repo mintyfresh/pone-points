9f4708a1bbaa2ce8376a91c102c58b2bbdd124d4
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-04 21:56:46 -0500

    Improve layout on large screens.

---
+++ app/components/give_points_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class GivePointsComponent < ApplicationComponent
+  # @param pone [Pone]
+  # @param current_pone [Pone, nil]
+  def initialize(pone:, current_pone:)
+    @pone         = pone
+    @current_pone = current_pone
+  end
+
+  # @return [Integer]
+  def giftable_points_count
+    return 0 if @current_pone.nil?
+
+    @giftable_points_count ||= @current_pone.giftable_points_count
+  end
+end
--- app/components/pone_profile_component.rb
+++ app/components/pone_profile_component.rb
@@ -2,16 +2,7 @@
 
 class PoneProfileComponent < ApplicationComponent
   # @param pone [Pone]
-  # @param current_pone [Pone, nil]
-  def initialize(pone:, current_pone:)
-    @pone         = pone
-    @current_pone = current_pone
-  end
-
-  # @return [Integer]
-  def giftable_points_count
-    return 0 if @current_pone.nil?
-
-    @giftable_points_count ||= @current_pone.giftable_points_count
+  def initialize(pone:)
+    @pone = pone
   end
 end
