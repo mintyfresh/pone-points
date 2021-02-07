fd8606f114c54c476795242f2d7dd469e3c5a7a3
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-06 20:40:21 -0500

    Add missing specs for pone policy.

--- spec/policies/pone_policy_spec.rb
+++ spec/policies/pone_policy_spec.rb
@@ -29,4 +29,18 @@ RSpec.describe PonePolicy, type: :policy do
       expect(policy).to permit(pone, other_pone)
     end
   end
+
+  permissions :integrations?, :change_password?, :update? do
+    it 'does not permit guests' do
+      expect(policy).not_to permit(nil, pone)
+    end
+
+    it 'permits themselves' do
+      expect(policy).to permit(pone, pone)
+    end
+
+    it 'does not permit for other pones' do
+      expect(policy).not_to permit(pone, other_pone)
+    end
+  end
 end
