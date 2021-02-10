2123ee0fa768fa5dc80931b1ae25f349156ca077
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2021-02-10 00:38:37 -0500

    Log message about expected files.

--- app/lib/message_bus.rb
+++ app/lib/message_bus.rb
@@ -13,12 +13,25 @@ class MessageBus
     clear_subscribers
 
     Dir[SUBSCRIBERS_PATH.join('**', '*_subscriber.rb')].each do |filename|
-      subscriber_class = Pathname.new(filename).relative_path_from(SUBSCRIBERS_PATH).to_s.chomp('.rb')
-      subscriber_class = subscriber_class.camelize.safe_constantize
+      subscriber_path = Pathname.new(filename).relative_path_from(SUBSCRIBERS_PATH)
+      next if subscriber_path.to_s == 'application_subscriber.rb'
 
+      load_subscriber(subscriber_path)
+    end
+  end
+
+  # @param subscriber_path [Pathname]
+  # @return [void]
+  def self.load_subscriber(subscriber_path)
+    subscriber_class = subscriber_path.to_s.chomp('.rb')
+    subscriber_class = subscriber_class.camelize.safe_constantize
+
+    if subscriber_class.is_a?(Class) && subscriber_class < ApplicationSubscriber
       subscriber_class.subscribed_patterns.each do |pattern|
         instance.add_subscriber(subscriber_class, pattern)
       end
+    else
+      Rails.logger.debug { "Expected #{subscriber_path} to contain a subscriber, but it did not. Skipping." }
     end
   end
 
